96712faeb9920aa2a774e3f587667d6772255e05
antelizflorez <est_aa_anteliz@fesc.edu.co>
2023-11-30 23:00:37 -0500

    appcata

---
+++ servidor/database/db.js
@@ -0,0 +1,10 @@
+import { Sequelize } from 'sequelize';
+
+//Se conecta a la base de datos usando sequlize, con los parametros de esta misma "nombre base de datos" "usuario" "constrasena"
+const db = new Sequelize('tienda', 'root', 'root', {
+    host:'localhost',
+    port: 3306,
+    dialect:'mysql'
+    
+});
+export default db;
